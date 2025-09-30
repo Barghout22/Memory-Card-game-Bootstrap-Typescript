ec85de31b36c168166584c842c32f04523c08e23
Barghout22 <barghoutmahmoud2@gmail.com>
2025-09-30 15:07:27 +0300

    Initial file layout Created the layout for the html file. included the bootstrap files. included the image and audio assets. added an autoplay that plays the game's main theme after 50 ms of the page loading.

---
+++ dist/main.js
@@ -0,0 +1,6 @@
+let audioPlayers = document.getElementsByTagName("audio");
+window.onload = function () {
+    //   setTimeout(()=>{ audioPlayers[0]?.play()},100);
+};
+export {};
+//# sourceMappingURL=main.js.map
